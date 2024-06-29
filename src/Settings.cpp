10916d66c93168ff6cf1e2ca1f48e2dea2d750d7
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2024-06-29 09:49:09 +0500

    Fix check settings was corrupted

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -2,42 +2,44 @@
 
 #include <EEPROM.h>
 
-namespace {
-constexpr uint32_t kSettingsWasInitedFlag = 0xABCDEFFE;
-} // namespace
-
 Settings::Settings() {
     int idx = 0;
+    uint32_t hash = 0;
 #define GET_SETTING(value)    \
     EEPROM.get(idx, (value)); \
-    idx += sizeof(value);
-
-    uint32_t checkInitFlag;
-    GET_SETTING(checkInitFlag);
-    if (checkInitFlag != kSettingsWasInitedFlag) {
-        lagTime = 0;
-        beepVolume = 6;
-        backlight = 25 * 5;
-        autoFinishViewMinutes = 3;
-        updateEEPROM();
-        return;
-    }
+    idx += sizeof(value);     \
+    hash += value;
 
     GET_SETTING(lagTime);
     GET_SETTING(beepVolume);
     GET_SETTING(backlight);
     GET_SETTING(autoFinishViewMinutes);
+
+    uint32_t realHash = hash;
+    uint32_t storedHash;
+    GET_SETTING(storedHash);
+
+    if (realHash != storedHash) {
+        lagTime = 0;
+        beepVolume = 5;
+        backlight = 3 * 5;
+        autoFinishViewMinutes = 3;
+        updateEEPROM();
+        return;
+    }
 }
 
 void Settings::updateEEPROM() {
     int idx = 0;
+    uint32_t hash = 0;
 #define PUT_SETTING(value)    \
     EEPROM.put(idx, (value)); \
-    idx += sizeof(value);
+    idx += sizeof(value);     \
+    hash += value;
 
-    PUT_SETTING(kSettingsWasInitedFlag);
     PUT_SETTING(lagTime);
     PUT_SETTING(beepVolume);
     PUT_SETTING(backlight);
     PUT_SETTING(autoFinishViewMinutes);
+    PUT_SETTING(hash);
 }
