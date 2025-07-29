0a486ff09494b9543a94cf1b5dfb286816002b30
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2025-07-29 09:04:10 +0500

    Make more constexpr functions in Time

--- DarkroomTimer/src/lib/DarkroomTimer/Time.h
+++ DarkroomTimer/src/lib/DarkroomTimer/Time.h
@@ -10,8 +10,8 @@ public:
     constexpr Time() : Time(0) {}
     constexpr explicit Time(int32_t ts) : m_ts(ts > INT16_MAX ? -1 : ts) {}
 
-    explicit operator bool() const { return m_ts; }
-    explicit operator int16_t() const { return m_ts; }
+    constexpr explicit operator bool() const { return m_ts; }
+    constexpr explicit operator int16_t() const { return m_ts; }
 
     Time operator*(double x) const {
         int32_t res = static_cast<int32_t>(lround(m_ts * x));
@@ -20,11 +20,12 @@ public:
 
         return Time{ static_cast<int16_t>(res) };
     }
-    Time operator*(int16_t x) const { return Time{ m_ts * x }; }
-    Time operator/(int n) const { return Time{ m_ts / n }; }
-    Time operator+(const Time& o) const { return Time{ m_ts + o.m_ts }; }
-    Time operator-(const Time& o) const { return Time{ m_ts - o.m_ts }; }
-    Time operator-() const { return Time{ -m_ts }; }
+
+    constexpr Time operator*(int16_t x) const { return Time{ m_ts * x }; }
+    constexpr Time operator/(int n) const { return Time{ m_ts / n }; }
+    constexpr Time operator+(const Time& o) const { return Time{ m_ts + o.m_ts }; }
+    constexpr Time operator-(const Time& o) const { return Time{ m_ts - o.m_ts }; }
+    constexpr Time operator-() const { return Time{ -m_ts }; }
 
     Time& operator*=(const int16_t x) {
         m_ts *= x;
@@ -36,19 +37,19 @@ public:
         return *this;
     }
 
-    bool operator<(const Time& o) const { return m_ts < o.m_ts; }
-    bool operator>(const Time& o) const { return m_ts > o.m_ts; }
-    bool operator==(const Time& o) const { return m_ts == o.m_ts; }
-    bool operator!=(const Time& o) const { return m_ts != o.m_ts; }
+    constexpr bool operator<(const Time& o) const { return m_ts < o.m_ts; }
+    constexpr bool operator>(const Time& o) const { return m_ts > o.m_ts; }
+    constexpr bool operator==(const Time& o) const { return m_ts == o.m_ts; }
+    constexpr bool operator!=(const Time& o) const { return m_ts != o.m_ts; }
 
-    int32_t toMillis() const { return m_ts * 100L; }
+    constexpr int32_t toMillis() const { return m_ts * 100L; }
     static Time fromMillis(int32_t ms) {
         int16_t ts = static_cast<int16_t>(ms / 100);
         return Time{ ts + (ms % 100 > 50) };
     }
 
-    int16_t secs() const { return m_ts / 10; }
-    uint8_t tenth() const { return m_ts % 10; }
+    constexpr int16_t secs() const { return m_ts / 10; }
+    constexpr uint8_t tenth() const { return m_ts % 10; }
 
     void getFormatedTime(char* buf, bool accurate = true, bool addZero = false) const;
 
