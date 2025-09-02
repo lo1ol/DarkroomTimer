326c032097db2f36bc2ef428a286a458eaef5c7e
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2025-09-02 10:05:49 +0500

    Speed up I2C communication by reducing number of transactions

--- DarkroomTimer/src/LcdImpl/I2C.cpp
+++ DarkroomTimer/src/LcdImpl/I2C.cpp
@@ -11,6 +11,10 @@
 
 namespace {
 
+// Simple I2C implementation
+// For more info:
+// https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+// Page 183
 void i2cInit() {
     TWSR = 0;
     TWBR = 72; // ~100kHz @16MHz
@@ -33,7 +37,7 @@ void i2cWrite(uint8_t data) {
         ;
 }
 
-static void i2cStop() {
+void i2cStop() {
     TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 }
 
@@ -43,15 +47,11 @@ void lcdSendNibble(uint8_t data, bool isData) {
     if (isData)
         data |= LCD_RS;
 
-    // pulse enable
+    // write data with pulse enable
     // https://github.com/arduino-libraries/LiquidCrystal/blob/8c18c69e9de2fb7d89e64098b1899064fee3ba06/src/LiquidCrystal.cpp#L303
-    i2cStart(LCD_ADDR << 1);
     i2cWrite(data | LCD_ENABLE);
-    i2cStop();
     delayMicroseconds(1);
-    i2cStart(LCD_ADDR << 1);
     i2cWrite(data & ~LCD_ENABLE);
-    i2cStop();
     delayMicroseconds(100);
 }
 
@@ -71,7 +71,9 @@ inline void lcdData(uint8_t value) {
 
 void Lcd::init() {
     i2cInit();
-    delay(50);
+    delay(50); // some time to wake up device
+
+    i2cStart(LCD_ADDR << 1);
     // set 4-bit mode
     lcdSendNibble(0x30, 0);
     delay(5);
@@ -84,16 +86,21 @@ void Lcd::init() {
     lcdCmd(0x06); // cursor to the left after printing
     lcdCmd(0x01); // clear display
     delay(2);     // delay for long clear display command
+    i2cStop();
 }
 
 void Lcd::setCursor(uint8_t c, uint8_t r) {
+    i2cStart(LCD_ADDR << 1);
     static uint8_t rowOffsets[] = { 0x00, 0x40 };
     lcdCmd(0x80 | (c + rowOffsets[r]));
+    i2cStop();
 }
 
 void Lcd::print(const char* str) {
+    i2cStart(LCD_ADDR << 1);
     while (*str)
         lcdData(*str++);
+    i2cStop();
 }
 
 #endif
