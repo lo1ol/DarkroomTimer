1951846a34e0abc140b59f7d097ff9a64b0db5f6
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2025-11-15 17:53:40 +0500

    Format CustomChar.h

--- DarkroomTimer/src/CustomChars.h
+++ DarkroomTimer/src/CustomChars.h
@@ -10,21 +10,26 @@ enum class Charset : uint8_t {
 };
 
 // Thank you for https://chareditor.com
-constexpr uint8_t kWrenchSymTopMatrix[] PROGMEM = { 0b01100, 0b11000, 0b11001, 0b11111,
-                                                    0b01110, 0b01100, 0b01100, 0b01100 };
-constexpr uint8_t kWrenchSymBottomMatrix[] PROGMEM = { 0b01100, 0b01100, 0b01100, 0b01110,
-                                                       0b11111, 0b10011, 0b00011, 0b00110 };
 
+// syms from main charset
+// clang-format off
+constexpr uint8_t kWrenchSymTopMatrix[] PROGMEM = {
+    0b01100, 0b11000, 0b11001, 0b11111, 0b01110, 0b01100, 0b01100, 0b01100
+};
+constexpr uint8_t kWrenchSymBottomMatrix[] PROGMEM = {
+    0b01100, 0b01100, 0b01100, 0b01110, 0b11111, 0b10011, 0b00011, 0b00110
+};
 constexpr uint8_t kSheetSymMatrix[] PROGMEM = {
     0b11100, 0b10110, 0b10111, 0b10001, 0b10001, 0b10001, 0b11111, 0b00000
 };
-constexpr uint8_t kPlaySymMatrix[] PROGMEM = { 0b10000, 0b11000, 0b11100, 0b11110, 0b11100, 0b11000, 0b10000, 0b00000 };
-
+constexpr uint8_t kPlaySymMatrix[] PROGMEM = {
+    0b10000, 0b11000, 0b11100, 0b11110, 0b11100, 0b11000, 0b10000, 0b00000
+};
 constexpr uint8_t kPauseSymMatrix[] PROGMEM = {
     0b00000, 0b11011, 0b11011, 0b11011, 0b11011, 0b11011, 0b11011, 0b00000
 };
+// clang-format on
 
-// syms from main charset
 #define kWrenchSymTop "\x80"
 #define kWrenchSymBottom "\x81"
 #define kSheetSym "\x82"
@@ -32,24 +37,34 @@ constexpr uint8_t kPauseSymMatrix[] PROGMEM = {
 #define kPauseSym "\x84"
 
 // syms from mode preview charset
-constexpr uint8_t kFolderBegin1SymMatrix[] PROGMEM = { 0b11100, 0b10010, 0b11111, 0b10000,
-                                                       0b10000, 0b10000, 0b10000, 0b11111 };
-constexpr uint8_t kFolderEnd1SymMatrix[] PROGMEM = { 0b00000, 0b00000, 0b11100, 0b00010,
-                                                     0b00010, 0b00010, 0b00010, 0b11110 };
-constexpr uint8_t kFolderBegin2SymMatrix[] PROGMEM = { 0b01110, 0b01001, 0b11111, 0b10000,
-                                                       0b10000, 0b10000, 0b10000, 0b11111 };
+// clang-format off
+constexpr uint8_t kFolderBegin1SymMatrix[] PROGMEM = {
+    0b11100, 0b10010, 0b11111, 0b10000, 0b10000, 0b10000, 0b10000, 0b11111
+};
+constexpr uint8_t kFolderEnd1SymMatrix[] PROGMEM = {
+    0b00000, 0b00000, 0b11100, 0b00010, 0b00010, 0b00010, 0b00010, 0b11110
+};
+constexpr uint8_t kFolderBegin2SymMatrix[] PROGMEM = {
+    0b01110, 0b01001, 0b11111, 0b10000, 0b10000, 0b10000, 0b10000, 0b11111
+};
 #define kFolderEnd2SymMatrix kFolderEnd1SymMatrix
-constexpr uint8_t kFolderBegin3SymMatrix[] PROGMEM = { 0b00111, 0b00100, 0b11111, 0b10000,
-                                                       0b10000, 0b10000, 0b10000, 0b11111 };
-constexpr uint8_t kFolderEnd3SymMatrix[] PROGMEM = { 0b00000, 0b10000, 0b11100, 0b00010,
-                                                     0b00010, 0b00010, 0b00010, 0b11110 };
-constexpr uint8_t kFolderBegin4SymMatrix[] PROGMEM = { 0b00011, 0b00010, 0b11111, 0b10000,
-                                                       0b10000, 0b10000, 0b10000, 0b11111 };
-constexpr uint8_t kFolderEnd4SymMatrix[] PROGMEM = { 0b10000, 0b01000, 0b11100, 0b00010,
-                                                     0b00010, 0b00010, 0b00010, 0b11110 };
+constexpr uint8_t kFolderBegin3SymMatrix[] PROGMEM = {
+    0b00111, 0b00100, 0b11111, 0b10000, 0b10000, 0b10000, 0b10000, 0b11111
+};
+constexpr uint8_t kFolderEnd3SymMatrix[] PROGMEM = {
+    0b00000, 0b10000, 0b11100, 0b00010, 0b00010, 0b00010, 0b00010, 0b11110
+};
+constexpr uint8_t kFolderBegin4SymMatrix[] PROGMEM = {
+    0b00011, 0b00010, 0b11111, 0b10000, 0b10000, 0b10000, 0b10000, 0b11111
+};
+constexpr uint8_t kFolderEnd4SymMatrix[] PROGMEM = {
+    0b10000, 0b01000, 0b11100, 0b00010, 0b00010, 0b00010, 0b00010, 0b11110
+};
 
-constexpr uint8_t kSubFolderPointerSymMatrix[] PROGMEM = { 0b00000, 0b01000, 0b01000, 0b01000,
-                                                           0b01110, 0b00000, 0b00000, 0b00000 };
+constexpr uint8_t kSubFolderPointerSymMatrix[] PROGMEM = {
+    0b00000, 0b01000, 0b01000, 0b01000, 0b01110, 0b00000, 0b00000, 0b00000
+};
+// clang-format on
 
 #define kFolderBegin1Sym "\x80"
 #define kFolderEnd1Sym "\x81"
@@ -67,19 +82,29 @@ constexpr uint8_t kSubFolderPointerSymMatrix[] PROGMEM = { 0b00000, 0b01000, 0b0
 #define kFolder4Str kFolderBegin4Sym kFolderEnd4Sym
 
 // syms from view charset
-constexpr uint8_t kLampSymMatrix[] PROGMEM = { 0b01110, 0b01110, 0b11111, 0b01010, 0b10001, 0b10001, 0b10001, 0b01110 };
-constexpr uint8_t kLampLight1SymMatrix[] PROGMEM = { 0b00000, 0b01010, 0b10101, 0b00100,
-                                                     0b00000, 0b00000, 0b00000, 0b11111 };
-constexpr uint8_t kLampLight2SymMatrix[] PROGMEM = { 0b00000, 0b00000, 0b01010, 0b10001,
-                                                     0b00100, 0b00100, 0b00000, 0b11111 };
-constexpr uint8_t kLampLight3SymMatrix[] PROGMEM = { 0b00000, 0b00000, 0b00000, 0b01010,
-                                                     0b10001, 0b10101, 0b00100, 0b11111 };
-constexpr uint8_t kLampLight4SymMatrix[] PROGMEM = { 0b00000, 0b00000, 0b00000, 0b00000,
-                                                     0b01010, 0b10001, 0b10101, 0b11111 };
-constexpr uint8_t kLampLight5SymMatrix[] PROGMEM = { 0b00000, 0b00000, 0b00000, 0b00000,
-                                                     0b00000, 0b01010, 0b10001, 0b11111 };
-constexpr uint8_t kLampLight6SymMatrix[] PROGMEM = { 0b00100, 0b01010, 0b00000, 0b00000,
-                                                     0b00000, 0b00000, 0b01010, 0b11111 };
+// clang-format off
+constexpr uint8_t kLampSymMatrix[] PROGMEM = {
+    0b01110, 0b01110, 0b11111, 0b01010, 0b10001, 0b10001, 0b10001, 0b01110
+};
+constexpr uint8_t kLampLight1SymMatrix[] PROGMEM = {
+    0b00000, 0b01010, 0b10101, 0b00100, 0b00000, 0b00000, 0b00000, 0b11111
+};
+constexpr uint8_t kLampLight2SymMatrix[] PROGMEM = {
+    0b00000, 0b00000, 0b01010, 0b10001, 0b00100, 0b00100, 0b00000, 0b11111
+};
+constexpr uint8_t kLampLight3SymMatrix[] PROGMEM = {
+    0b00000, 0b00000, 0b00000, 0b01010, 0b10001, 0b10101, 0b00100, 0b11111
+};
+constexpr uint8_t kLampLight4SymMatrix[] PROGMEM = {
+    0b00000, 0b00000, 0b00000, 0b00000, 0b01010, 0b10001, 0b10101, 0b11111
+};
+constexpr uint8_t kLampLight5SymMatrix[] PROGMEM = {
+    0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b01010, 0b10001, 0b11111
+};
+constexpr uint8_t kLampLight6SymMatrix[] PROGMEM = {
+    0b00100, 0b01010, 0b00000, 0b00000, 0b00000, 0b00000, 0b01010, 0b11111
+};
+// clang-format on
 
 #define kLampSym "\x80"
 #define kLampLight1Sym "\x81"
